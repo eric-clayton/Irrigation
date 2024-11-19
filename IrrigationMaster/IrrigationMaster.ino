e5242b24ba5879d69d0f1fe4a12f272e608ebb2a
Eric Clayton <68301170+eric-clayton@users.noreply.github.com>
2024-11-19 14:29:53 -0500

    Refact for water level

--- IrrigationMaster/IrrigationMaster.ino
+++ IrrigationMaster/IrrigationMaster.ino
@@ -2,40 +2,68 @@
 #include <Wire.h>
 #include <LiquidCrystal_PCF8574.h>
 
-LiquidCrystal_PCF8574 lcd(0x27);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+
+
+// Rain pump connected to digital pin 9
+const byte RAIN_PUMP_PIN = 9;
+// Well pump connected to digital pin 4       
+const byte WELL_PUMP_PIN = 4; 
+// 20 PSI
+const byte LOW_PRESSURE = 20;
+// 40 PSI  
+const byte HIGH_PRESSURE = 40;
+// Inversed beause it is measured as distance from water to the top of tank. Measured in decimeters
+const byte LOW_WATER_LEVEL = 30; // 9.8 feet
+
+// if the water level is low or the flow was 0 do not retry for 3 hr 
+const unsigned long RAIN_REST_TIME = 10800000;
+// 30 min will require a manual reset. 
+const unsigned long OVERRUN_TIME = 1800000; 
+// 24 hr
+const unsigned long FILTER_FLUSH_REST_TIME  = 86400000;
+// 5 seconds to flush
+const unsigned long FILTER_FLUSH_TIME = 5000;
+
+// set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_PCF8574 lcd(0x27);  
 //LCD connections: SCL > A5,  SDA > A4
 //CAN BUS connections: CS > D10, SO > D12, SI > D11, SCK > D13,  INT > D2
 
-const byte RpumpPin = 9;        // Rain pump connected to digital pin 9
-const byte WpumpPin = 4;        // Well pump connected to digital pin 4
+unsigned long pumpStart;
+unsigned long rainRestStart;
+unsigned long flushRestStart;
+unsigned long filterFlushStart;
 
-const byte LP = 20;  // 30 PSI
-const byte HP = 40;  // 40 PSI
+bool isOverrun = false;
+bool isFlowZero = false;
+bool isWellResting = false;
+bool isFlushingTimerActive = false;
 
-unsigned long TimeMark[9];
-unsigned long TimeElaps[9];
+bool isRainRestTimerActive = false;
+bool isWaterLevelTimerActive = false;
+bool isPressureTimerActive = false;
+bool isFlushRestTimerActive = false;
 
-const unsigned long primRst = 10800000; // 3 hr (1h = 3,600,000ms)
-const unsigned long primtime = 30000; 
-const unsigned long overruntime = 1800000; // 30m (30m = 1,800,000ms) will require a manual reset.
-const unsigned long FilterFushIntval  = 86400000; // 24 hr
-bool pumpHOLD;
-bool RpumpHOLD;
-bool WpumpHOLD;
-bool overrun;
-byte vl;
+// Flow is zero but water level is above minimum
+bool flowWaterLevelMismatch = false;
+byte waterLevelMismatch = 255;
+// Save the current pump pin 255 is undefined pin and pumps should be off
+byte currentPump = 255;
 
 void setup() {
   // put your setup code here, to run once:
   //Serial.begin(9600);
   lcd.begin(16, 2); // initialize the lcd
-   delay(1000);
+  delay(1000);
   lcd.setBacklight(255);
   lcd.home(); 
   lcd.noBlink();
   lcd.noCursor();
   lcd.clear();
-  lcd.print("Starting");
+  lcd.print("Starting...");
+  lcd.setCursor(0, 1);
+  lcd.print("Intializing CAN");
   delay(2000);
   lcd.clear();
   // start the CAN bus at 500 kbps
@@ -44,214 +72,312 @@ void setup() {
     lcd.print("Com CAN failed!");
     while (1);
   }
-  pinMode(RpumpPin, OUTPUT);  // sets the digital pin 9 as output
-  pinMode(WpumpPin, OUTPUT);  // sets the digital pin 4 as output
-  digitalWrite(RpumpPin, HIGH);
-  digitalWrite(WpumpPin, HIGH);
-  }
+  lcd.print("CAN initialized");
+  delay(2000);
+  // sets the digital pin 9 as output
+  pinMode(RAIN_PUMP_PIN, OUTPUT);
+   // sets the digital pin 4 as output  
+  pinMode(WELL_PUMP_PIN, OUTPUT);
+  // Turn off pumps 
+  digitalWrite(RAIN_PUMP_PIN, HIGH);
+  digitalWrite(WELL_PUMP_PIN, HIGH);
+}
 
 void loop() {
-   // put your main code here, to run repeatedly:
-  byte Perrlev;
-  byte pump = RpumpPin;
-  byte tn = 4;
-  char *pumpname[] = {"Rain Pump", "Well Pump" };
-  int i = 0; 
-  if (vl) {
-    pump = WpumpPin;
-  //  i++;
-    tn = 5;
+  byte pressure = getPressure();
+
+  byte waterLevel = 255;
+
+  if (isRainRestTimerActive) {
+    isRainRestTimerActive = !hasTimeRunOut(rainRestStart, RAIN_REST_TIME);
   }
-
-  if (!pumpHOLD && !overrun) {
-    do {
-        Perrlev = MaintainPresure(pump, pumpname[vl]);
-        lcd.clear();
-        lcd.print(pumpname[vl]);
-        lcd.setCursor(0, 1);
-        if (Perrlev == 2) lcd.print("Prim Fail");
-        if (Perrlev == 3) {
-          lcd.print("Overrun");
-          overrun = true;
-        }
-        delay(5000);
-        RpumpHOLD = true;
-      } while (Perrlev > 4);
+  if (!isRainRestTimerActive) {
+    waterLevel = getWaterLevel();
   }
-
-  if (timer(tn, primRst, "Monostable")) pumpHOLD = false; //Stop trying Rain pump for pumpRst time
-
-
-
-
-if (vl) {
-  lcd.clear();
-  lcd.print("ALL PUMPS FAILED");
-  lcd.setCursor(0, 1);
-  if (Perrlev == 2) lcd.print("Prim Fail");
-  if (Perrlev == 3) lcd.print("Overrun");
-  delay(1000);
-  while (1); //Halt Loop
-} 
-
-
-vl++;
-}
-
-
-
-byte* GetDisplayPress(char pumpname[]){
-  byte waterPress = recive(0x2);
-
-  if (waterPress == 255) {
-    lcd.clear();
-    lcd.print("ERROR No Data");
-    lcd.setCursor(0, 1);
-    lcd.print("from Remote End");
-    delay(1000);
-  }else {
-    lcd.clear();
-    lcd.print(pumpname);
-    lcd.setCursor(0, 1);
-    lcd.print("Pressure ");
-    lcd.print(waterPress);
-    lcd.print(" PSI");
-    delay(1000);
-  }
-    return waterPress;
-  }
-short GetFlow(char pumpname[]) {
-  byte prevPressure = 255;
-  byte nextPressure = 255;
-  byte MAX_TRIES = 5;
-  byte currTry = 0;
-  while(prevPressure == 255) {
-    if(currTry >= MAX_TRIES) {
-      return 255;
+  // if pump on
+  if (currentPump != 255) {
+    if (pressure >= HIGH_PRESSURE || (currentPump == RAIN_PUMP_PIN && waterLevel >= LOW_WATER_LEVEL)) {
+      shutDownPumps();
     }
-    prevPressure = GetDisplayPress(pumpname);
-    currTry++;
   }
-  currTry = 0;
-  while(nextPressure == 255 && currTry < MAX_TRIES) {
-    if(currTry >= MAX_TRIES) {
-      return 255;
-    }
-    nextPressure = GetDisplayPress(pumpname);
-    currTry++;
-  }
-  short flow = nextPressure - prevPressure;
-  return flow;
-}
-byte MaintainPresure(byte pump, char pumpname[]){
-  //byte Pump;
-  //if (pumpname == "Rain Pump") Pump = RpumpPin;
-  //if (pumpname == "Well Pump") Pump = WpumpPin;
-  byte Perrorlev; //0 = pres low, 1 = pres hi, 2 = prim fail, 3 = exced runtme, 4 = leak
-  delay(1000);
-
-  do {
-    byte Readp = GetDisplayPress(pumpname);
-    delay(100);
-    if (Readp <= LP) {
-      digitalWrite(pump, LOW);   // running pump
-      if (timer(2, primtime, "Monostable")){
-        // Prime pump time delay
-        Perrorlev = 2;
-        digitalWrite(pump, HIGH);   // stop pump
+  // Pump is off. If pressure is low turn on pump  
+  else if (pressure <= LOW_PRESSURE) {
+    if (!isRainRestTimerActive) {
+      // if water level is too low set rain rest to true
+      if (waterLevel >= LOW_WATER_LEVEL) {
+        isRainRestTimerActive = true;
+        rainRestStart = millis();
+        if (!isWellResting) {
+          currentPump = WELL_PUMP_PIN;
+        } 
       }
       else {
-        Perrorlev = 0;
+        currentPump = RAIN_PUMP_PIN;
       }
     }
-    // Max pressure
-    else if (Readp >= HP){
-      timer(2, 0, "resetMSTimer");
-      timer(3, 0, "resetMSTimer");
-      digitalWrite(pump, HIGH);   // stop pump
-      Perrorlev = 1;
-      if (timer(7, FilterFushIntval, "Monostable")) filterflush();
-    }
-    // Pressure normal
-    else {
-      if (timer(3, overruntime, "Monostable")) Perrorlev = 3;
-      timer(2, 0, "resetMSTimer");
+    else if (!isWellResting) {
+      currentPump = WELL_PUMP_PIN;
+    } 
+    if(currentPump != 255) {
+      // if the filter is flushing turn it off before turning on the pump
+      if(isFlushingTimerActive) {
+        isFlushRestTimerActive = filterFlush(true); // override set to true to immediately turn off filter flush
+        flushRestStart = millis();
       }
-    } while (Perrorlev < 2);
-  return Perrorlev;
+      digitalWrite(currentPump, LOW);
+      pumpStart = millis();
+    }
+  }
+  // Pump is off. If filter flush is not resting start flushing
+  else {
+    // Check if rest is over
+    if (isFlushRestTimerActive) {
+      isFlushRestTimerActive  = !hasTimeRunOut(flushRestStart, FILTER_FLUSH_REST_TIME);
+    }
+    // if rest is over start flush
+    if(!isFlushRestTimerActive) {
+      isFlushRestTimerActive = filterFlush(false); // override is off so we dont end the flush early
+      if(isFlushRestTimerActive) {
+          flushRestStart = millis();  // flush rest timer has started because flush has ended
+      }
+    }
+  }
+  // failsafes if the pumps are still on
+  if (currentPump != 255 || areAnyPumpsOn()) {
+      isOverrun = hasTimeRunOut(pumpStart, OVERRUN_TIME);
+      if (isOverrun) {
+        // num of hours times length of hour in ms (24 hours)
+        const unsigned long REST_TIME = (3.6e+6 * 24);
+        shutDownPumps();
+        printToLcd("Error pump ran", "too long" );
+        // shutdown for rest time
+        delay(REST_TIME);
+      }
+      isFlowZero = getFlow() <= 0 ? true : false;
+      if (isFlowZero) {    
+        if(!isRainRestTimerActive && currentPump == RAIN_PUMP_PIN && waterLevel < LOW_WATER_LEVEL) {
+          flowWaterLevelMismatch = true;
+          waterLevelMismatch = waterLevel;
+          isRainRestTimerActive = true;
+          rainRestStart = millis();
+        }
+        else if(currentPump == WELL_PUMP_PIN) {
+          isWellResting = true;
+        }
+        shutDownPumps();    
+      }
+  }
+  if(flowWaterLevelMismatch) {
+    printToLcd("Error flow/wtr", " lvl mismatch");
+    delay(1000);
+    lcd.clear();
+    lcd.print("Water level: ");
+    lcd.setCursor(0, 1);
+    lcd.print(waterLevelMismatch);
+    delay(1000);
+  }
+  if(isWellResting) {
+    printToLcd("Error no flow", "from well pump");
+    delay(1000);
+  }
 }
+byte getPressure(){
+  unsigned long getPressureStart = millis();
+  const unsigned long GET_PRESSURE_TIME = 6000;
+  const int PRESSURE_ID = 0x2;
+  byte pressure = 255;
+  do {
+    pressure = CANreceive(PRESSURE_ID);
 
-void filterflush() {
-  lcd.clear();
-  lcd.print("Flushing Filter");
-  CANsend(0x7, 1);
-  delay(5000);
-  CANsend(0x7, 0);
+    if(pressure != 255) {
+      return pressure;
+    }
+    else if (hasTimeRunOut(getPressureStart, GET_PRESSURE_TIME)) {
+      shutDownPumps();
+      printToLcd("Read timeout", "pressure");
+      while(1);
+    }
+  } while (pressure == 255);
 }
+byte getWaterLevel(){
+  unsigned long getWaterLevelStart = millis();
+  const unsigned long GET_WATER_LEVEL_TIME = 6000;
+  const int WATER_LEVEL_ID = 0x3;
+  byte waterLevel = 255;
+  do {
+    waterLevel = CANreceive(WATER_LEVEL_ID);
 
-bool timer(byte x, unsigned long ms, char mode[13]){
-  byte sc;
-  if (mode == "Astable") {sc = 0;}
-  if (mode == "Monostable") {sc = 1;}
-  if (mode == "resetMSTimer") {sc = 2;}
-  if (TimeMark[x] == 0) TimeMark[x] = millis();
-  TimeElaps[x] = millis() - TimeMark[x];
-    switch (sc){
-      case 0:  //Astable
-        if (TimeElaps[x] < ms){ //time is up//timeout = true;
-          return true;
-        }
-        else if(TimeElaps[x] < ms*2){
-          return false;
-        }
-         else{ 
-          TimeMark[x] = millis();
-        }
-        break;
-      case 1:  //Monostable
-        if (TimeElaps[x] >= ms){ //time is up//timeout = true;
-          TimeElaps[x] = 0;
-          TimeMark[x] = 0;//reset
-          return true;
-        }
-        else
-        {
-          return false;
-        }
-      break;
-      case 2:  //resetMSTimer
-        TimeMark[x] = 0;
-        TimeElaps[x] = 0;
-        return false;
-        break;   
-    }   
-} 
+    if (waterLevel != 255) {
+      return waterLevel;
+    }
+    else if (hasTimeRunOut(getWaterLevelStart, GET_WATER_LEVEL_TIME)) {
+      shutDownPumps();
+      printToLcd("Read timeout", "Water level");
+      while(1);
+    }
+  } while (waterLevel == 255);
+}
+unsigned long timeElapsed(unsigned long startTime) {
+  // unsigned long max value;
+  const unsigned long MAX_ULONG = 0xFFFFFFFF;
+  unsigned long currTime = millis();
+  // Arduino timer restarted in between capturing the elapsed time
+  if (currTime < startTime) {
+    int timeElapsedBeforeReset = MAX_ULONG - startTime; // Time between start time and the max value of a ulong
+    return timeElapsedBeforeReset + currTime; // currTime is time elapsed after the clock reset to 0. Adding the time before reset and after gives total time elapsed
+  }
+  else {
+    return currTime - startTime;
+  }
+}
+// returns if clock has finished
+bool hasTimeRunOut(unsigned long startTime, unsigned long duration) {
+  if(timeElapsed(startTime) >= duration) {
+    return true;
+  }
+  return false;
+}
+short getFlow() {
+  short prevPressure = (short)getPressure();
+  delay(500);
+  short currPressure = (short)getPressure();
   
-void CANsend(char addr, byte val){
-  CAN.beginPacket(addr);
+  if(currPressure == 255 || prevPressure == 255) {
+    return 255;
+  }
+  short flow = currPressure - prevPressure;
+  return flow;
+}
+// override is to turn off fliter flush immediately /
+// Return true if filter flush is done
+bool filterFlush(bool override) {
+  // Send id for pressure
+  const int FILTER_ID = 0x7;
+  if(!isFlushingTimerActive) {
+    if(override) {
+      return true;
+    }
+    lcd.clear();
+    lcd.print("Flushing Filter");
+    delay(500);
+    CANsend(FILTER_ID, 1);
+    filterFlushStart = millis();
+    isFlushingTimerActive = true;
+  }
+  else if(hasTimeRunOut(filterFlushStart, FILTER_FLUSH_TIME) || override) {
+    isFlushingTimerActive = false;
+    CANsend(FILTER_ID, 0);
+    lcd.clear();
+    lcd.print("Done Flushing");
+    delay(500);
+  }
+  return !isFlushingTimerActive;
+}
+void CANsend(int id, byte val){
+  CAN.beginPacket(id);
   CAN.write(val);
   CAN.endPacket();
-  delay(100);
 }
 
-byte recive(char addr) {
-  //int packetSize = CAN.parsePacket();
-//return 255;
-   if (CAN.parsePacket()) {    
-     while (CAN.available()) {
-      if (CAN.packetId() == addr){ 
-         // lcd.clear();
-         // lcd.print("CANReading somthing");
-       delay(100);
-       return CAN.read();
-       
+byte CANreceive(int id) {
+  if (CAN.parsePacket()) {    
+    if (CAN.available()) {
+      if (CAN.packetId() == (long)id){ 
+        return CAN.read();
       }
     } 
-  }else {return 255;}
- 
-
   }
+  return 255;
+}
+// return false if errror occurs trying to shut down the specified pin
+void shutDownPumps() {
+  digitalWrite(WELL_PUMP_PIN, HIGH);
+  digitalWrite(RAIN_PUMP_PIN, HIGH);
+  currentPump = 255;
+}
+void printToLcd(const char* line1, const char* line2) {
+  lcd.clear();
+  lcd.print(line1);
+  lcd.setCursor(0, 1);
+  lcd.print(line2);
+}
+bool areAnyPumpsOn(){
+  bool wellPumpOn = digitalRead(WELL_PUMP_PIN) == LOW;
+  bool rainPumpOn = digitalRead(RAIN_PUMP_PIN) == LOW;
+  return wellPumpOn || rainPumpOn;
+}
+// bool timer(byte x, unsigned long ms, char mode[13]){
+//   byte sc;
+//   if (mode == "Astable") {sc = 0;}
+//   if (mode == "Monostable") {sc = 1;}
+//   if (mode == "resetMSTimer") {sc = 2;}
+//   if (TimeMark[x] == 0) TimeMark[x] = millis();
+//   TimeElaps[x] = millis() - TimeMark[x];
+//     switch (sc){
+//       case 0:  //Astable
+//         if (TimeElaps[x] < ms){ //time is up//timeout = true;
+//           return true;
+//         }
+//         else if(TimeElaps[x] < ms*2){
+//           return false;
+//         }
+//          else{ 
+//           TimeMark[x] = millis();
+//         }
+//         break;
+//       case 1:  //Monostable
+//         if (TimeElaps[x] >= ms){ //time is up//timeout = true;
+//           TimeElaps[x] = 0;
+//           TimeMark[x] = 0;//reset
+//           return true;
+//         }
+//         else
+//         {
+//           return false;
+//         }
+//       break;
+//       case 2:  //resetMSTimer
+//         TimeMark[x] = 0;
+//         TimeElaps[x] = 0;
+//         return false;
+//         break;   
+//     }   
+// } 
+  
+// byte MaintainPressure(byte pump, char pumpname[]){
+//   byte Perrorlev; //0 = pres low, 1 = pres hi, 2 = prim fail, 3 = exced runtme, 4 = leak
 
+//   do {
+//     byte Readp = getPressure();
+//     delay(100);
+//     if (Readp <= LOW_PRESSURE) {
+//       digitalWrite(pump, LOW);   // running pump
+//       if (timer(2, PRIME_TIME, "Monostable")){
+//         // Prime pump time delay
+//         Perrorlev = 2;
+//         digitalWrite(pump, HIGH);   // stop pump
+//       }
+//       else {
+//         Perrorlev = 0;
+//       }
+//     }
+//     // Max pressure
+//     else if (Readp >= HIGH_PRESSURE){
+//       timer(2, 0, "resetMSTimer");
+//       timer(3, 0, "resetMSTimer");
+//       digitalWrite(pump, HIGH);   // stop pump
+//       Perrorlev = 1;
+//       if (timer(7, FILTER_FLUSH_TIME, "Monostable")) filterflush();
+//     }
+//     // Pressure normal
+//     else {
+//       if (timer(3, OVERRUN_TIME, "Monostable")) Perrorlev = 3;
+//       timer(2, 0, "resetMSTimer");
+//     }
+//     } while (Perrorlev < 2);
+//   return Perrorlev;
+// }
+// return true if filter is done flushing
 
 
 
--- IrrigationRemoteEnd/IrrigationRemoteEnd.ino
+++ IrrigationRemoteEnd/IrrigationRemoteEnd.ino
@@ -1,81 +1,117 @@
-#include <Arduino.h>
 #include <CAN.h>
 //CAN BUS connections: CS > D10, SO > D12, SI > D11, SCK > D13,  INT > D2
 
-const int WellPresPin = A7;   // Well pressure sensor analog pin A7
-const int ComError_Pin = 5;   // CAN comuication bord failure connected to digital pin 5
-const byte FilterRelayPin = 6;
+// Error Codes for CAN communication failure CAN light will blink for each interval listed
+// The intialize CAN failed 1s
+const short INITIALIZE_ERROR_INTERVAL = 1000;
+
+// Send id for pressure
+const int PRESSURE_ID = 0x2;
+// Send id for water level
+const int WATER_LEVEL_ID = 0x3;
+// Receive id for pressure
+const int FILTER_ID = 0x7;
+
+// Analog pins
+// Water pressure sensor analog pin A7
+const byte WATER_PRESSURE_PIN_A = A7;
+// Water level echo analog pin A6
+const byte WATER_LEVEL_ECHO_PIN_A = A6;
+// Water level trig analog pin A5
+const byte WATER_LEVEL_TRIG_PIN_A = A5;
+
+// Digital pins
+// CAN communication board failure digital pin 5
+const byte COM_ERROR_PIN_D = 5;
+// Fliter Relay digital pin 6
+const byte FILTER_RELAY_PIN_D = 6;
 
 void setup() {
-  // put your setup code here, to run once:
-  //Serial.begin(9600);
-  //delay(2000);
-  //Serial.println("serial started");
+  // Connect can communication error as output
+  pinMode(COM_ERROR_PIN_D, OUTPUT);
+  // Connect filter relay as output
+  pinMode(FILTER_RELAY_PIN_D, OUTPUT);
+  // Sets the trig Pin as an OUTPUT
+  pinMode(WATER_LEVEL_TRIG_PIN_A, OUTPUT);
+  // Sets the echo Pin as an INPUT
+  pinMode(WATER_LEVEL_ECHO_PIN_A, INPUT); 
   
-  pinMode(ComError_Pin, OUTPUT);      // sets the digital pin 5 as output
-  pinMode(FilterRelayPin, OUTPUT);  // sets the digital pin 4 as output
-  digitalWrite(FilterRelayPin, HIGH);
+  digitalWrite(FILTER_RELAY_PIN_D, HIGH);
 
-  // start the CAN bus at 500 kbps
+  // Start the CAN bus at 500 kbps
   if (!CAN.begin(500E3)) {
-    //Serial.println("Starting CAN failed!");
-    while (1) { //stops program
-      digitalWrite(ComError_Pin, LOW);
-      delay(700);
-      digitalWrite(ComError_Pin, HIGH);
-      delay(700);
-    } 
+    // intialize CAN error;
+    communicationErrorLight(INITIALIZE_ERROR_INTERVAL);
+  }
+  else {
+    // Initialize CAN is good
+    digitalWrite(COM_ERROR_PIN_D, HIGH);
   }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  digitalWrite(ComError_Pin, HIGH); //program started
-   
-  CANsend(0x2, Pressure(WellPresPin));
-  //Serial.println(recive(0x7));
-  
-  if (recive(0x7) == 1) {
-    digitalWrite(FilterRelayPin, LOW);
+  CANsend(PRESSURE_ID, getPressure());
+  CANsend(WATER_LEVEL_ID, getWaterLevel());
+  if (CANreceive(FILTER_ID) == 1) {
+    digitalWrite(FILTER_RELAY_PIN_D, LOW);
   }
   else {
-    digitalWrite(FilterRelayPin, HIGH);
+    digitalWrite(FILTER_RELAY_PIN_D, HIGH);
   }
+  delay(1000);
+}
+byte getPressure(){
+  const byte NUM_OF_READS = 20;
+  int pressureTotal = 0;
+  byte counter = 0;
 
+  while (counter < NUM_OF_READS){
+    // read the pump pressure input pin
+    pressureTotal += analogRead(WATER_PRESSURE_PIN_A);
+    counter++;
+  }
+  int pressureReading = pressureTotal / NUM_OF_READS; // Average pressure reading
+
+  // Map the voltage reading to psi
+  byte pressure = map(pressureReading, 100, 880, 0, 100);
+  return pressure;
+}
+byte getWaterLevel() {
+  // Clears the trig Pin condition
+  digitalWrite(WATER_LEVEL_TRIG_PIN_A, LOW);
+  delayMicroseconds(2);
+  // Sets the trig Pin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(WATER_LEVEL_TRIG_PIN_A, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(WATER_LEVEL_TRIG_PIN_A, LOW);
+  // Reads the echo Pin, returns the sound wave travel time in microseconds
+  long duration = pulseIn(WATER_LEVEL_ECHO_PIN_A, HIGH);
+  // Calculating the distance
+  int distance = duration * 0.00343 / 2; // Speed of sound wave divided by 2 (go and back) decimeters
+  return (byte)distance;
 }
 
-byte Pressure(int Sensor){
-  int PSen;
-  int pst = 0;
-  byte var = 0;
-//Serial.println(analogRead(Sensor));
-  while(var < 20){
-    PSen = analogRead(Sensor);   // read the pump pressure input pin
-    var++;
-    pst = pst + PSen;
-  }
-  
-  PSen = pst / 20;
-  byte Readp = map(PSen, 100, 880, 0, 100);
-  return Readp;
-}
-  
-void CANsend (char addr, byte val){
-  CAN.beginPacket(addr);
+void CANsend (int id, byte val) {
+  CAN.beginPacket(id);
   CAN.write(val);
   CAN.endPacket();
-  delay(100);
 }
 
-byte recive(char addr) {
-  int packetSize = CAN.parsePacket();
-  if (packetSize) {    
-    while (CAN.available()) {
-      if (CAN.packetId() == addr){ 
-       return CAN.read();
-       delay(1000);
+byte CANreceive(int id) {
+  if (CAN.parsePacket()) {    
+    if (CAN.available()) {
+      if (CAN.packetId() == (long)id){ 
+        return CAN.read();
       }
-    }
+    } 
+  }
+  return 255;
+}
+void communicationErrorLight(unsigned long interval) {
+  while(1) {
+      digitalWrite(COM_ERROR_PIN_D, LOW);
+      delay(interval);
+      digitalWrite(COM_ERROR_PIN_D, HIGH);
+      delay(interval);
   }
 }
